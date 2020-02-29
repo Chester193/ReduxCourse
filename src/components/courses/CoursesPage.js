d26fb1c22e549d091a442fed0aa3a398a772c778
Chester193 <46018874+Chester193@users.noreply.github.com>
2020-02-29 20:11:19 +0100

    add store config

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,8 +1,36 @@
 import React from "react";
 
 class CoursePage extends React.Component {
+  state = {
+    course: {
+      title: ""
+    }
+  };
+
+  handlechange = event => {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({
+      course
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    alert(this.state.course.title);
+  };
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Courses</h2>
+        <h3>Add course</h3>
+        <input
+          type="text"
+          onChange={this.handlechange}
+          value={this.state.course.title}
+        ></input>
+        <input type="submit" value="Save"></input>
+      </form>
+    );
   }
 }
 
